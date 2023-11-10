1803c76df951825dcb59fe842412020f1c133a4b
Sefinek <contact@sefinek.net>
2023-11-10 06:26:47 +0100

    Add endpoint /

--- routes/Index.js
+++ routes/Index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const DownloadBenefitsController = require('../controllers/Stella-Mod-Plus/DownloadBenefits.js');
 
+router.get('/', (req, res) => res.send(`<h1>Mirror #${process.env.MIRROR_ID}</h1>It works normal.`));
+
 const basePath = '/benefits/genshin-impact-reshade';
 const logicBasePath = `${basePath}/receive/:userId/:key`;
 router.get(`${logicBasePath}/download`, DownloadBenefitsController.download);
