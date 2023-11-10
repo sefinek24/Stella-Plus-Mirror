3439a072390f30dcae4f8c7f30d92e40074dfd7d
Sefinek <contact@sefinek.net>
2023-11-10 06:22:55 +0100

    Fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
 
 		log_date_format       : 'HH:mm:ss.SSS DD.MM.YYYY',
 		merge_logs            : true,
-		log_file		      : '/home/sefinek/logs/www/mirror.sefinek.net/combined.log',
-		out_file              : '/home/sefinek/logs/www/mirror.sefinek.net/out.log',
-		error_file            : '/home/sefinek/logs/www/mirror.sefinek.net/error.log',
+		log_file		      : '/home/ubuntu/logs/www/mirror.sefinek.net/combined.log',
+		out_file              : '/home/ubuntu/logs/www/mirror.sefinek.net/out.log',
+		error_file            : '/home/ubuntu/logs/www/mirror.sefinek.net/error.log',
 
 		max_restarts          : 4,
 		restart_delay         : 4000,
