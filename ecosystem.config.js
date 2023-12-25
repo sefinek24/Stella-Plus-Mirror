0bd776f95c7a9aaef61a894a674e2a3c2f0cb1bf
Sefinek <contact@sefinek.net>
2023-12-25 08:32:41 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,33 @@
 module.exports = {
 	apps: [{
-		name                  : 'mirror',
-		script                : './index.js',
+		name: 'mirror',
+		script: './index.js',
 
-		log_date_format       : 'HH:mm:ss.SSS DD.MM.YYYY',
-		merge_logs            : true,
-		log_file		      : '/home/ubuntu/logs/www/mirror.sefinek.net/combined.log',
-		out_file              : '/home/ubuntu/logs/www/mirror.sefinek.net/out.log',
-		error_file            : '/home/ubuntu/logs/www/mirror.sefinek.net/error.log',
+		// Configuration options
+		exec_mode: 'fork',
+		max_memory_restart: '1G',
 
-		max_restarts          : 4,
-		restart_delay         : 4000,
-		wait_ready            : true,
+		// Monitoring changes in files and restarting the application
+		watch: false,
+		ignore_watch: ['.git', 'node_modules', 'logs', '.eslintrc.js', 'ecosystem.config.js'],
 
-		// instances             : 'max',
-		// exec_mode             : 'cluster',
+		// Logging settings
+		log_date_format: 'HH:mm:ss.SSS DD.MM.YYYY',
+		merge_logs: true,
+		log_file: '/home/ubuntu/logs/www/mirror/combined.log',
+		out_file: '/home/ubuntu/logs/www/mirror/out.log',
+		error_file: '/home/ubuntu/logs/www/mirror/error.log',
+
+		// Application restart policy
+		wait_ready: true,
+		autorestart: true,
+		max_restarts: 5,
+		restart_delay: 4000,
+		min_uptime: 13000,
+
+		// Environment variables
+		env: {
+			NODE_ENV: 'production',
+		},
 	}],
 };
