89845800252e213c5c45d1cad242e15129412ef9
Sefinek <contact@sefinek.net>
2024-03-02 02:52:03 +0100

    Update `mongoose.js`

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
+connect(process.env.MONGODB_URL).then(() => {
 	console.info('Connected to the database');
 }).catch(err => {
 	console.error('Failed to connect to the database', err);
 	process.exit(1);
 });
 
-const db = mongoose.connection;
-db.on('connected', () => console.info('MongoDB connected successfully!'));
-db.on('disconnected', () => console.warn('MongoDB disconnected!'));
-db.on('error', err => console.error('MongoDB connection error:', err));
+connection.on('connected', () => console.info('MongoDB connected successfully!'));
+connection.on('disconnected', () => console.warn('MongoDB disconnected!'));
+connection.on('error', err => console.error('MongoDB connection error:', err));
