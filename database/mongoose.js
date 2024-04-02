d4168e470ced571d61e35ea6d5a0a25815beeb47
Sefinek Actions <contact@sefinek.net>
2024-04-02 23:43:51 +0200

    Update middlewares

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,12 +1,19 @@
 const { connect, connection } = require('mongoose');
 
-connect(process.env.MONGODB_URL).then(() => {
-	console.info('Connected to the database');
-}).catch(err => {
+connect(process.env.MONGODB_URL).catch(err => {
 	console.error('Failed to connect to the database', err);
 	process.exit(1);
 });
 
-connection.on('connected', () => console.info('MongoDB connected successfully!'));
-connection.on('disconnected', () => console.warn('MongoDB disconnected!'));
-connection.on('error', err => console.error('MongoDB connection error:', err));
+connection.on('connected', async () => {
+	console.info('Successfully connected to the database');
+});
+
+connection.on('disconnected', () => {
+	console.warn('MongoDB disconnected!');
+});
+
+connection.on('error', err => {
+	console.error('MongoDB connection error:', err);
+	process.exit(1);
+});
--- middlewares/timeout.js
+++ middlewares/timeout.js
@@ -2,6 +2,6 @@ const timeout = require('express-timeout-handler');
 
 module.exports = () => timeout.handler({
 	timeout: 12000,
-	onTimeout: (req, res) => res.status(503).send('503 ERROR'),
+	onTimeout: (req, res) => res.status(503).json({ success: false, status: 503, message: 'Request timeout. Service unavailable.' }),
 	disable: ['write', 'setHeaders', 'send', 'json', 'end'],
 });
