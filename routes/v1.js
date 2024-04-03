e2e5e97478b95d3037bfb8bbbeb9b7b95b47e97a
Sefinek Actions <contact@sefinek.net>
2024-04-03 21:37:42 +0200

    Fix endpoint

--- routes/v1.js
+++ routes/v1.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const limiter = require('./scripts/v1/ratelimit.js');
 const DownloadBenefitsController = require('../controllers/v1/launcher/DownloadBenefits.js');
 
-router.get('/genshin-stella-mod/patrons/benefits/download', limiter, passport.authenticate('mirror', { session: false }), DownloadBenefitsController);
+router.get('/stella-mod-plus/benefits/download', limiter, passport.authenticate('mirror', { session: false }), DownloadBenefitsController);
 router.head('/test/speedtest', (req, res) => res.sendStatus(200));
 
 module.exports = router;
