b9e36aa6a48f480c52e61ba4537ff6e982366413
Sefinek <contact@sefinek.net>
2024-10-10 11:26:50 +0200

    Fix: tier-2_favorite-cat.zip

--- controllers/Patron-Center/scripts/determineZipPath.js
+++ controllers/Patron-Center/scripts/determineZipPath.js
@@ -7,7 +7,7 @@ const benefitsDir = process.env.NODE_ENV === 'production'
 module.exports = benefitId => {
 	switch (benefitId) {
 	case 2:
-		return path.join(benefitsDir, 'tier-2_favorite-kitten.zip');
+		return path.join(benefitsDir, 'tier-2_favorite-cat.zip');
 	case 3: case 4: case 5:
 		return path.join(benefitsDir, 'tier-3_safety-kitten.zip');
 	default:
