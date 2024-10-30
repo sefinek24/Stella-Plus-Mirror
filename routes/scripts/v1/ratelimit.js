113e89b70c581c7f074fae4d7bae1e3c62ef4d13
Sefinek <contact@sefinek.net>
2024-10-30 22:25:59 +0100

    Update ratelimit.js

--- routes/scripts/v1/ratelimit.js
+++ routes/scripts/v1/ratelimit.js
@@ -1,13 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
-	windowMs: 4 * 60 * 1000,
-	limit: 6,
+	windowMs: 5 * 60 * 1000,
+	limit: 4,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
 
-	skip: req => req.ip === '::ffff:127.0.0.1',
-
-	message: { success: false, status: 429, message: 'Too many requests. Please try again later.' },
-	statusCode: 429
+	skip: req => req.ip === '::ffff:127.0.0.1' && process.env.NODE_ENV === 'development',
+	handler: (req, res) => res.status(429).json({ success: false, status: 429, message: 'Too many requests. Please try again later.' })
 });
